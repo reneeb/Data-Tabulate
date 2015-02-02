347e4bd4e4943e82a7a70f47b0f845a59c07ddd9
reneeb <github@renee-baecker.de>
2015-02-02 10:01:55 +0100

    remove pod boilerplate, because some of it is generated by Dist::Zilla

--- lib/Data/Tabulate.pm
+++ lib/Data/Tabulate.pm
@@ -4,15 +4,7 @@ use warnings;
 use strict;
 use Carp;
 
-=head1 NAME
-
-Data::Tabulate - Table generation!
-
-=head1 VERSION
-
-Version 0.06
-
-=cut
+# ABSTRACT: Table generation!
 
 our $VERSION = '0.06';
 
@@ -302,57 +294,5 @@ sub _data{
     return @{$self->{data}};
 }
 
-=head2 
-
-=head1 AUTHOR
-
-Renee Baecker, C<< <module at renee-baecker.de> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-C<bug-data-tabulate at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-Tabulate>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Data::Tabulate
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Data-Tabulate>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Data-Tabulate>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Data-Tabulate>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Data-Tabulate>
-
-=back
-
-=head1 ACKNOWLEDGEMENTS
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2006 - 2010 Renee Baecker, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms of Artistic License 2.0.
-
-=cut
-
 1; # End of Data::Tabulate
+
